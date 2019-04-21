822d5a50ff55a6da7b8559d6e71896aa61b54fc7
Krishnaswamy Subramanian <jskswamy@gmail.com>
2019-04-22 00:03:43 +0530

    Show only 3 questions instead of 9

--- src/game/Embedded.js
+++ src/game/Embedded.js
@@ -1,15 +1,38 @@
 import * as React from "react";
 import Page from "../common/Page";
 import Qa from '../games/Qa';
+import {publishGameMessage} from "../lib/socket";
 
 export default class Embedded extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleComplete = this.handleComplete.bind(this);
+  }
+
+  handleComplete(status) {
+    const {game: {Name: name}} = this.props;
+    publishGameMessage(name, {action: status});
+  }
+
+  renderGame() {
+    const {game: {Name: name, Metadata: metadata = {}}} = this.props;
+    switch (name) {
+      case "quiz":
+        const {questions = []} = metadata;
+        return <Qa options={questions} onComplete={this.handleComplete}/>;
+      case "small":
+        return <h1>Why so serious</h1>;
+      default:
+        return <h1>Call volunteer</h1>;
+    }
+  }
 
   render() {
-    const {game} = this.props;
+    const {game: {Title: title}} = this.props;
     return (
         <Page>
-          <h1>{game.Title}</h1>   
-          {game.Name === 'quiz' && <Qa options={game.Metadata.questions} />}
+          <h1>{title}</h1>
+          {this.renderGame()}
         </Page>
     )
   }
--- src/games/Qa.js
+++ src/games/Qa.js
@@ -1,112 +1,118 @@
-import React, { Component } from 'react';
-import { QaModel } from "./qa/QaModel";
+import React, {Component} from 'react';
+import {QaModel} from "./qa/QaModel";
 import _ from "lodash";
-import { publishGameMessage } from "../lib/socket";
 import './Qa.css'
 
+let completedTimer;
+
 class Qa extends Component {
-    constructor(props, context) {
-        super(props, context);
-        let qas = [];
-        let shuffledQuestions = _.shuffle(props.options);
+  constructor(props, context) {
+    super(props, context);
+    let qas = [];
+    let shuffledQuestions = _.shuffle(props.options);
 
-        _.each(shuffledQuestions.slice(1, 10), (question) => {
-            qas.push(new QaModel(question.id, question.question, question.options, question.answer))
-        });
+    _.each(shuffledQuestions.slice(1, 4), (question) => {
+      qas.push(new QaModel(question.id, question.question, question.options, question.answer))
+    });
 
-        this.state = {
-            questions: qas,
-            currentQuestion: qas[getRandomArbitrary(0, qas.length - 1)],
-            selectedOption: "",
-            isOver: false,
-            correctAnswers: 0,
-            count: qas.length
-        };
+    this.state = {
+      questions: qas,
+      currentQuestion: qas[getRandomArbitrary(0, qas.length - 1)],
+      selectedOption: "",
+      isOver: false,
+      correctAnswers: 0,
+      count: qas.length
+    };
+  }
+
+  handleSubmit = () => {
+    if (!this.state.selectedOption) {
+      return
     }
-    handleSubmit = () => {
-        if (!this.state.selectedOption) {
-            return
-        }
 
-        let totalCorrect = this.state.correctAnswers
-
-        if (this.state.selectedOption === this.state.currentQuestion.answer) {
-            totalCorrect++
-        }
-
-        let questions = this.removeQuestion(this.state.currentQuestion.id);
-        this.setState({
-            questions: questions,
-            currentQuestion: questions[getRandomArbitrary(0, questions.length - 1)],
-            isOver: _.isEmpty(questions),
-            correctAnswers: totalCorrect,
-            selectedOption: ""
-        })
-        if (_.isEmpty(questions)) {
-            if (this.state.correctAnswers === this.state.count) { publishGameMessage('quiz', 'completed'); }
-            else {
-                publishGameMessage('quiz', 'failed');
-            }
-        }
-    };
-
-    handleOptionChange = (event) => {
-        this.setState({ selectedOption: event.target.value })
-    };
-
-    removeQuestion = (questionID) => {
-        let questions = this.state.questions;
-        _.remove(questions, (question) => {
-            return question.id === questionID
-        });
-
-        return questions;
-    };
-
-    render() {
-        return (
-            <div className='quizWrapper'>
-                {!this.state.isOver &&
-                    <React.Fragment>
-                        <form>
-                            <p className='question'>
-                                {this.state.currentQuestion.question}
-                            </p>
-                            {this.state.currentQuestion.options.map((option, index) => {
-                                console.log(index);
-                                return <div className="form-check" key={index}>
-                                    <input
-                                        id={option}
-                                        type="radio"
-                                        name="react-tips"
-                                        value={option}
-                                        className="form-check-input"
-                                        onChange={this.handleOptionChange}
-                                        checked={option === this.state.selectedOption}
-                                        key={index}
-                                    />
-                                    <label for={option}>{option}</label>
-                                </div>
-                            })}
-                        </form>
-                        <button onClick={this.handleSubmit}>Submit</button>
-                    </React.Fragment>
-                }
-                {this.state.isOver &&
-                    <h1>you answered {this.state.correctAnswers}/{this.state.count} right</h1>
-                }
-            </div>
-
-        );
+    let totalCorrect = this.state.correctAnswers;
+    if (this.state.selectedOption === this.state.currentQuestion.answer) {
+      totalCorrect++
     }
+
+    let questions = this.removeQuestion(this.state.currentQuestion.id);
+    const isOver = _.isEmpty(questions);
+    this.setState({
+      questions: questions,
+      currentQuestion: questions[getRandomArbitrary(0, questions.length - 1)],
+      isOver,
+      correctAnswers: totalCorrect,
+      selectedOption: ""
+    });
+
+    if (_.isEmpty(questions)) {
+      const {onComplete = _.noop} = this.props;
+      const status = this.state.correctAnswers === this.state.count ? 'completed' : 'failed';
+      if (isOver) {
+        clearTimeout(completedTimer);
+        completedTimer = setTimeout(() => {
+          onComplete(status);
+        }, 3000)
+      }
+    }
+  };
+
+  handleOptionChange = (event) => {
+    this.setState({selectedOption: event.target.value})
+  };
+
+  removeQuestion = (questionID) => {
+    let questions = this.state.questions;
+    _.remove(questions, (question) => {
+      return question.id === questionID
+    });
+
+    return questions;
+  };
+
+  render() {
+    return (
+        <div className='quizWrapper'>
+          {!this.state.isOver &&
+          <React.Fragment>
+            <form>
+              <p className='question'>
+                {this.state.currentQuestion.question}
+              </p>
+              {this.state.currentQuestion.options.map((option, index) => {
+                return <div className="form-check" key={index}>
+                  <input
+                      id={option}
+                      type="radio"
+                      name="react-tips"
+                      value={option}
+                      className="form-check-input"
+                      onChange={this.handleOptionChange}
+                      checked={option === this.state.selectedOption}
+                      key={index}
+                  />
+                  <label htmlFor={option}>{option}</label>
+                </div>
+              })}
+            </form>
+            <button onClick={this.handleSubmit}>Submit</button>
+          </React.Fragment>
+          }
+          {this.state.isOver &&
+          <h1>you answered {this.state.correctAnswers}/{this.state.count} right</h1>
+          }
+        </div>
+
+    );
+  }
 }
 
 export default Qa;
 
 function getRandomArbitrary(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 
