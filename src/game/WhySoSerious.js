dd2a609a4b22818d7b5ca5d52fa3cd1d8301bb8d
Krishnaswamy Subramanian <jskswamy@gmail.com>
2019-04-22 05:15:16 +0530

    Format WhySoSerious

--- src/game/WhySoSerious.js
+++ src/game/WhySoSerious.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import * as faceapi from 'face-api.js';
 import Webcam from "react-webcam";
 import orderBy from 'lodash'
@@ -8,50 +8,55 @@ class WhySoSerious extends Component {
   setRef = webcam => {
     this.webcam = webcam;
   };
-  constructor(){
-    super();
+
+  constructor(props) {
+    super(props);
     this.state = {
       expresion: "",
       timerId: null
-    }
+    };
     this.webcam = React.createRef();
   }
+
   analyse = async () => {
     console.log("called");
     this.setState({expresion: ""});
-    var image = new Image();
+    const image = new Image();
     image.src = this.webcam.getScreenshot();
     const detectionsWithExpressions = await faceapi.detectAllFaces(image).withFaceExpressions();
-    console.log(detectionsWithExpressions)
-    if(detectionsWithExpressions && detectionsWithExpressions.length !== 0) {
+    console.log(detectionsWithExpressions);
+    if (detectionsWithExpressions && detectionsWithExpressions.length !== 0) {
       this.setState(
-        {
-          image: image.src,
-          expresion: orderBy(detectionsWithExpressions[0].expressions, ['probability'], ['desc'])[0]['expression']
-        }
-        );
-      if(this.state.expresion === "neutral") {
-          this.setState({oldImage: image.src});
+          {
+            image: image.src,
+            expresion: orderBy(detectionsWithExpressions[0].expressions, ['probability'], ['desc'])[0]['expression']
+          }
+      );
+      if (this.state.expresion === "neutral") {
+        this.setState({oldImage: image.src});
       }
     }
-  }
-  async componentDidMount(){
+  };
+
+  async componentDidMount() {
     await faceapi.loadFaceDetectionModel("models")
     await faceapi.loadFaceExpressionModel("models")
     await faceapi.loadSsdMobilenetv1Model('models')
-    const timerId =setInterval(() => this.analyse(), 1000);
+    const timerId = setInterval(() => this.analyse(), 1000);
     this.setState({timerId});
   }
+
   getSuccess() {
     clearInterval(this.state.timerId);
     return (
-      <div className="container">
-        <img alt="you won" className="joker" src="https://media.giphy.com/media/KEVNWkmWm6dm8/giphy.gif" />
-        <img alt="happy" className="happy" src={this.state.image} />
-        <img alt="not happy" className="nothappy" src={this.state.oldImage} />
-      </div>
+        <div className="container">
+          <img alt="you won" className="joker" src="https://media.giphy.com/media/KEVNWkmWm6dm8/giphy.gif"/>
+          <img alt="happy" className="happy" src={this.state.image}/>
+          <img alt="not happy" className="nothappy" src={this.state.oldImage}/>
+        </div>
     )
   }
+
   render() {
     const videoConstraints = {
       width: 350,
@@ -59,20 +64,23 @@ class WhySoSerious extends Component {
       facingMode: 'user',
     };
     return (
-      <div className="App">
-        <div className="App-header">
-          <Webcam
-            audio={false}
-            height={350}
-            ref={this.setRef}
-            screenshotFormat="image/jpeg"
-            width={350}
-            videoConstraints={videoConstraints}
-          />
-          <div className="expression">{this.state.expresion ? this.state.expresion : ""}</div>
-          <button onClick={() => {this.analyse()}} >Detect</button>
+        <div className="App">
+          <div className="App-header">
+            <Webcam
+                audio={false}
+                height={350}
+                ref={this.setRef}
+                screenshotFormat="image/jpeg"
+                width={350}
+                videoConstraints={videoConstraints}
+            />
+            <div className="expression">{this.state.expresion ? this.state.expresion : ""}</div>
+            <button onClick={() => {
+              this.analyse()
+            }}>Detect
+            </button>
+          </div>
         </div>
-      </div>
     );
   }
 }
