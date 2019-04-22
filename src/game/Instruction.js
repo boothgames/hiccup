f2f14c28f13f2b888666a6b257fb47f58ff87156
Krishnaswamy Subramanian <jskswamy@gmail.com>
2019-04-22 05:52:18 +0530

    Show progress bar when showing  instruction

--- src/game/Instruction.js
+++ src/game/Instruction.js
@@ -3,30 +3,40 @@ import Page from "../common/Page";
 import './dashboard.css'
 import _ from "lodash";
 import {countDownTimer} from "../lib/timer";
+import ProgressBar from "react-progress-bar-plus";
 
 export default class Instruction extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      game: 'vr',
-    }
+    this.state = {progress: 0};
   }
 
   componentDidMount() {
     const {onRead = _.noop} = this.props;
-    countDownTimer({
-      completed: () => {
-        onRead();
+    const duration = 5000;
+    this.timer = countDownTimer({
+      tick: ({ms}) => {
+        const progress = 100 - Math.round((((duration - ms) / duration) * 100));
+        this.setState({progress});
       },
-      duration: 5000
+      completed: onRead,
+      duration,
+      refresh: 100,
     });
+  }
+
+  componentWillUnmount() {
+    this.timer.stop();
+    this.timer = null;
   }
 
   render() {
     const {games} = this.props;
+    const {progress} = this.state;
     return (
         <Page>
           <div className="gooey-Ex-large">
+            <ProgressBar percent={progress} spinner={false}/>
             <h2 className='gameName-info'>Instruction</h2>
             <ul>{_.map(games, ({Name: name, Title: title}) => {
               return (
--- src/lib/timer.js
+++ src/lib/timer.js
@@ -1,8 +1,8 @@
 import _ from "lodash";
 import Stopwatch from "timer-stopwatch";
 
-export const countDownTimer = ({tick = _.noop, completed = _.noop, duration = 3000}) => {
-  const timer = new Stopwatch(duration, {refreshRateMS: 1000});
+export const countDownTimer = ({tick = _.noop, completed = _.noop, duration = 3000, refresh = 1000}) => {
+  const timer = new Stopwatch(duration, {refreshRateMS: refresh});
   timer.onTime(tick);
   timer.onDone(completed);
   timer.start();
