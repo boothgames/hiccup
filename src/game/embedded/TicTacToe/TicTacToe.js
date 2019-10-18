63b769c4ff0223d4067542372d67bac961605262
Nithya Natarajan <nithyanatarajn@gmail.com>
2019-10-18 17:39:12 +0530

    Fix js warnings
    Signed-off-by: Nithya Natarajan <nithyanatarajn@gmail.com>

--- src/game/embedded/TicTacToe/TicTacToe.js
+++ src/game/embedded/TicTacToe/TicTacToe.js
@@ -22,11 +22,11 @@ class TicTacToe extends Component {
     clicked(box) {
         if(this.gameState.gameEnded || this.gameState.gameLocked) return;
 
-        if(this.gameState.board[box.dataset.square] == '') {
+        if(this.gameState.board[box.dataset.square] === '') {
             this.gameState.board[box.dataset.square] = this.gameState.turn;
             box.innerText = this.gameState.turn;
 
-            this.gameState.turn = this.gameState.turn == 'X' ? 'O' : 'X';
+            this.gameState.turn = this.gameState.turn === 'X' ? 'O' : 'X';
 
                 this.gameState.totalMoves++;
         }
@@ -35,7 +35,7 @@ class TicTacToe extends Component {
 
         var result = this.checkWinner();
         const {onComplete} = this.props;
-        if(result == 'X') {
+        if(result === 'X') {
             this.gameState.gameEnded = true;
             setTimeout(() => {
                 onComplete('completed');
@@ -44,7 +44,7 @@ class TicTacToe extends Component {
                 winner: 'X',
                 winnerLine: 'You won'
             });
-        } else if(result == 'O') {
+        } else if(result === 'O') {
             this.gameState.gameEnded = true;
             setTimeout(() => {
                 onComplete('failed');
@@ -53,7 +53,7 @@ class TicTacToe extends Component {
                 winner: 'O',
                 winnerLine: 'You lose'
             });
-        } else if(result == 'draw') {
+        } else if(result === 'draw') {
             this.gameState.gameEnded = true;
             setTimeout(() => {
                 onComplete('failed');
@@ -64,12 +64,12 @@ class TicTacToe extends Component {
             })
         }
 
-        if(this.gameState.turn == 'O' && !this.gameState.gameEnded) {
+        if(this.gameState.turn === 'O' && !this.gameState.gameEnded) {
             this.gameState.gameLocked = true;
             setTimeout(()=> {
                 do {
                     var random = Math.floor(Math.random()*9);
-                } while(this.gameState.board[random] != '');
+                } while(this.gameState.board[random] !== '');
                 this.gameState.gameLocked = false;
                 this.clicked(document.querySelectorAll('.square')[random]);
             }, 1000);
@@ -82,12 +82,12 @@ class TicTacToe extends Component {
         var moves = [[0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 1, 2], [3, 4, 5], [6, 7, 8]];
         var board = this.gameState.board;
         for(let i=0;i<moves.length;i++) {
-            if(board[moves[i][0]] == board[moves[i][1]] && board[moves[i][1]] == board[moves[i][2]])
+            if(board[moves[i][0]] === board[moves[i][1]] && board[moves[i][1]] === board[moves[i][2]])
                 return board[moves[i][0]];
         }
 
         console.log(this.gameState.totalMoves);
-        if(this.gameState.totalMoves == 9) {
+        if(this.gameState.totalMoves === 9) {
             return 'draw';
         }
     }
