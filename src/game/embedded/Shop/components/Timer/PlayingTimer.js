27c423245f677820e7e936dfc205e21d67cba5c7
Sukanya R <sukanya.r.aux@go-jek.com>
2020-06-30 16:07:15 +0530

    [Sukanya/Sarath] make height of logo and banner fixed.

--- src/game/embedded/Shop/components/Timer/PlayingTimer.js
+++ src/game/embedded/Shop/components/Timer/PlayingTimer.js
@@ -24,16 +24,6 @@ const PlayingTimer = props => {
     })
   };
 
-  const UrgeWithPleasureComponent = () => (
-    <CountdownCircleTimer
-      isPlaying
-      duration={10}
-      colors={[['#004777', 0.33], ['#F7B801', 0.33], ['#A30000']]}
-    >
-      {({ remainingTime }) => remainingTime}
-    </CountdownCircleTimer>
-  )
-
   return (
     <CountdownCircleTimer
       isPlaying
